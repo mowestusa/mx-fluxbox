da8277ffca79c44bdb0a1e0b2498f9df09289bf4
jerry3904 <stormyjerry@gmail.com>
2019-11-23 09:25:14 -0500

    added fluxox installed check to install script

--- mxfb_install.sh
+++ mxfb_install.sh
@@ -1,16 +1,22 @@
-    #################################################
-    # make sure you first install fluxbox and idesk #
-    #################################################
-
 #this script will set up fluxbox on the MX installation
 #right-click the downloaded script > Properties > Permissions
 #and check the box Allow to run as a program
 #then open a terminal is the same folder as the script
-#and enter: sh install.sh
+#and enter: sh mxfb_install.sh
 
 --------------
 
 #set up fluxbox
+
+if [ ! -d "$HOME/.fluxbox" ]; then
+   yad --text="Before running this script you must install FLUXBOX and IDESK. 
+   
+Exiting..." --title="Before starting!" --geometry=400x10 --timeout=5
+fi
+
+if [ ! -d "$HOME/.fluxbox" ]; then
+exit
+fi
 
 wget https://github.com/jerry3904/mx-fluxbox/archive/master.zip -P ~/Downloads/
 
@@ -33,6 +39,10 @@ cp ~/.fluxbox/components/idesk/.ideskrc ~/.ideskrc
 cp ~/.fluxbox/components/.fehbg ~/.fehbg
 
 #set up the conky
+
+if [ ! -d "$HOME/.conky/MX-BritPanel/" ]; then
+    mkdir -p "$HOME/.conky/MX-BritPanel"
+fi
 
 cp ~/.fluxbox/components/MX-BritPanel/conkyrc_fb ~/.conky/MX-BritPanel/conkyrc_fb
 
