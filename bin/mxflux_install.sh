bb5ebbe5e63eb3ddbc70aad87b960ce1821e27df
jerry3904 <stormyjerry@gmail.com>
2019-12-03 10:59:51 -0500

    loaded the correct file this time...

--- bin/mxflux_install.sh
+++ bin/mxflux_install.sh
@@ -1,49 +1,63 @@
-#this script will set up fluxbox on the MX installation
-#right-click the downloaded script > Properties > Permissions
-#and check the box Allow to run as a program
-#then open a terminal is the same folder as the script
-#and enter: sh mxflux_install.sh
+#!/bin/bash
 
---------------
-#test that user is not root
+#mx-fluxbox-install
 
-if [ "$(id -u)" = "0" ]; then
-   echo "You must run this script as regular user" 1>&2
-   exit 1
+##ASSUMPTIONS##
+#1) that fluxbox and idesk have been installed
+#2) that this script has been placed in /usr/bin/mxflux/
+#3) that the tarball from https://github.com/jerry3904/mx-fluxbox has been extracted and placed in 
+#/usr/share/mxflux/
+
+TEXTDOMAINDIR=/usr/share/locale 
+export TEXTDOMAIN="mxflux"
+
+MESSAGE=$"This will install into your Home folder components that MX-flux requires in order to run.  Any existing fluxbox configuration will be backed up as <i>fluxbox_BAK</i>."
+END_MSG=$"To start a fluxbox session: log out, select <i>fluxbox</i> from the pull-down menu in the upper right corner, and log back in."
+
+##begin message box
+yad --text="
+
+
+<b>$MESSAGE</b>" --title=$"OK to proceed?" --window-icon=/usr/share/icons/mxflux.png --geometry=400x40 --text-align=center 
+##end message box
+
+if [ ! "$?" = "0" ]; then
+    exit 0
 fi
 
 #set up fluxbox
 
-wget https://github.com/jerry3904/mx-fluxbox/archive/master.zip -P ~/Downloads/
+if [ -d "$HOME/.fluxbox" ]; then
+    mv $HOME/.fluxbox $HOME/.fluxbox_BAK
+fi
 
-cd ~/Downloads/	
-
-unzip -d ~/ master.zip
-
-cd ~/
-
-mv mx-fluxbox-master/ .fluxbox/ 
-
-mv ~/.fluxbox/pixmaps/mxflux.png /usr/share/icons/
+cp -r /usr/share/mxflux/.fluxbox "$HOME"
 
 #set up idesk
 
-mkdir ~/.idesktop/
+if [ ! -d "$HOME/.idesktop/" ]; then
+    mkdir -p "$HOME"/.idesktop/
+fi
 
-cp ~/.fluxbox/components/idesk/fluxhelp.lnk ~/.idesktop/fluxhelp.lnk
+cp /usr/share/mxflux/.fluxbox/components/idesk/fluxhelp.lnk "$HOME"/.idesktop/fluxhelp.lnk
 
-cp ~/.fluxbox/components/idesk/.ideskrc ~/.ideskrc
+cp /usr/share/mxflux/.fluxbox/components/idesk/.ideskrc "$HOME"/.ideskrc
 
-cp ~/.fluxbox/components/.fehbg ~/.fehbg
+cp /usr/share/mxflux/.fluxbox/components/.fehbg "$HOME"/.fehbg
 
 #set up the conky
 
 if [ ! -d "$HOME/.conky/MX-BritPanel/" ]; then
-    mkdir -p "$HOME/.conky/MX-BritPanel"
+    mkdir -p "$HOME/.conky/MX-BritPanel/"
 fi
 
-cp ~/.fluxbox/components/MX-BritPanel/conkyrc_fb ~/.conky/MX-BritPanel/conkyrc_fb
+cp /usr/share/mxflux/.fluxbox/components/MX-BritPanel/conkyrc_fb "$HOME"/.conky/MX-BritPanel/conkyrc_fb
 
-#finish
- 
-yad --text="To see the result: log out, select fluxbox from the pull-down menu in the upper right corner, and log back in." --title="All done!" --width=400 
+sleep 2
+
+#start concluding message
+yad --text="
+
+
+<b>$END_MSG</b>" --title=$"All done!" --window-icon=/usr/share/icons/mxflux.png --geometry=400x40 --text-align=center 
+#end concluding message
